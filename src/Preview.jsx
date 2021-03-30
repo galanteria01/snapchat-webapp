20bbfceae9f8a03c4bea6f1375649821a992272b
Shoury Sharma <shanuu12e@gmail.com>
2021-03-30 16:48:26 +0530

    Completed camera solutions

--- src/Preview.jsx
+++ src/Preview.jsx
@@ -1,22 +1,52 @@
 import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom';
-import { selectCameraImage } from './features/cameraSlice';
+import { resetCameraImage, selectCameraImage } from './features/cameraSlice';
 import './Preview.css'
+import CloseIcon from '@material-ui/icons/Close';
+import TextFieldsIcon from '@material-ui/icons/TextFields';
+import CreateIcon from '@material-ui/icons/Create';
+import NoteIcon from '@material-ui/icons/Note';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import CropIcon from '@material-ui/icons/Crop';
+import TimerIcon from '@material-ui/icons/Timer';
+import SendIcon from '@material-ui/icons/Send';
 
 const Preview = () => {
 
+    
+
     const cameraImage = useSelector(selectCameraImage);
     const history = useHistory();
+    const dispatch = useDispatch();
     useEffect( () => {
         if(!cameraImage) {
             history.replace('/');
         }
     } ,[cameraImage,history] )
+    const closePreview = () => {
+        dispatch(resetCameraImage())
+    }
 
     return (
         <div className="preview">
+            <CloseIcon onClick={closePreview} className="preview__close" />
+            <div className="preview__toolbar">
+                <TextFieldsIcon />
+                <CreateIcon />
+                <NoteIcon />
+                <MusicNoteIcon />
+                <AttachFileIcon />
+                <CropIcon />
+                <TimerIcon />
+            </div>
             <img src={cameraImage} alt=""/>
+            <div className="preview__footer">
+                <h2>Send now</h2>
+                <SendIcon fontSize="small" className="preview__sendIcon" />
+            </div>
+
         </div>
     )
 }
